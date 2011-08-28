f5ec93ba7cc11347635a21c0913e5aba6d9d19c8
Artemev Yurii <shatter@li.ru>
2011-08-28 23:49:04 +0400

    install.sql fixed

--- install.sql
+++ install.sql
@@ -1,77 +1,84 @@
+--
+-- Table structure `prefix_forum_category`
+--
+
 CREATE TABLE IF NOT EXISTS `prefix_forum_category` (
-  `category_id` int(11) unsigned NOT NULL auto_increment,
+  `category_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `category_title` varchar(255) NOT NULL,
-  PRIMARY KEY  (`category_id`),
+  PRIMARY KEY (`category_id`),
   KEY `category_id` (`category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-INSERT INTO `prefix_forum_category` (`category_id`, `category_title`) VALUES
-(1, 'Категория 1'),
-(2, 'Категория 2');
-
+--
+-- Table structure `prefix_forum_list`
+--
 
 CREATE TABLE IF NOT EXISTS `prefix_forum_list` (
-  `forum_id` int(11) unsigned NOT NULL auto_increment,
-  `forum_parent_id` int(11) unsigned NOT NULL default '0',
+  `forum_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `forum_parent_id` int(11) unsigned NOT NULL DEFAULT '0',
   `category_id` int(11) unsigned NOT NULL,
-  `forum_title` varchar(255) character set utf8 NOT NULL default '',
-  `forum_url` varchar(255) character set utf8 NOT NULL default '',
-  `forum_moder` varchar(250) character set utf8 NOT NULL,
+  `forum_title` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `forum_description` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `forum_url` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `forum_moder` varchar(250) CHARACTER SET utf8 NOT NULL,
   `forum_sort` int(11) NOT NULL,
-  PRIMARY KEY  (`forum_id`),
+  `post_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `topic_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`forum_id`),
   KEY `forum_id` (`forum_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-INSERT INTO `prefix_forum_list` (`forum_id`, `forum_parent_id`, `category_id`, `forum_title`, `forum_url`, `forum_moder`, `forum_sort`) VALUES
-(1, 0, 1, 'Тестовый форум', 'test', '1 2', 3),
-(2, 0, 2, 'Тестовый форум 2', 'test2', '1 2', 4),
-(3, 0, 1, 'Тестовый форум cat 1', 'testc1', '1 2', 1),
-(4, 0, 2, 'Тестовый форум cat 2', 'testc2', '1 2', 2);
+--
+-- Table structure `prefix_forum_posts`
+--
 
 CREATE TABLE IF NOT EXISTS `prefix_forum_posts` (
-  `post_id` int(11) unsigned NOT NULL auto_increment,
+  `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `forum_id` int(11) unsigned NOT NULL,
   `topic_id` int(11) unsigned NOT NULL,
   `user_id` int(11) unsigned NOT NULL,
   `post_date` datetime NOT NULL,
   `post_text` text NOT NULL,
   `post_text_source` text NOT NULL,
-  PRIMARY KEY  (`post_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=52 ;
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `prefix_forum_posts` (`post_id`, `topic_id`, `user_id`, `post_date`, `post_text`, `post_text_source`) VALUES
-(40, 48, 1, '2011-05-05 09:29:57', 'Сейчас он должен добавиться так, как надо, иначе я застрелюсь.', 'Сейчас он должен добавиться так, как надо, иначе я застрелюсь.'),
-(41, 49, 1, '2011-05-05 12:30:11', 'фывфывфывфывфывфывфывфыв', 'фывфывфывфывфывфывфывфыв'),
-(42, 50, 1, '2011-05-05 14:38:39', 'asdasdasdasdasdasdasdasdasdasd', 'asdasdasdasdasdasdasdasdasdasd'),
-(43, 51, 1, '2011-05-05 14:42:21', 'dasdasdasdasdasd', 'dasdasdasdasdasd'),
-(44, 52, 1, '2011-05-05 14:44:03', 'sdasdasdasdasd', 'sdasdasdasdasd'),
-(45, 53, 1, '2011-05-05 14:44:14', 'ddddddddddd', 'ddddddddddd'),
-(46, 54, 1, '2011-05-05 14:51:35', '312', '312'),
-(47, 55, 1, '2011-05-05 14:51:42', '3123123123', '3123123123'),
-(48, 56, 1, '2011-05-05 14:51:49', '523453453453453454', '523453453453453454'),
-(49, 57, 1, '2011-05-05 14:51:56', '35sdfsdfasdfasdf', '35sdfsdfasdfasdf'),
-(50, 58, 1, '2011-05-05 15:20:28', '4565768567546', '4565768567546'),
-(51, 58, 2, '2011-05-05 15:21:09', '12312312312312312312', '12312312312312312312');
+-- --------------------------------------------------------
+
+--
+-- Table structure `prefix_forum_read`
+--
+
+CREATE TABLE IF NOT EXISTS `prefix_forum_read` (
+  `topic_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `date_read` datetime NOT NULL,
+  `post_id_last` int(11) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure `prefix_forum_topics`
+--
 
 CREATE TABLE IF NOT EXISTS `prefix_forum_topics` (
-  `topic_id` int(11) unsigned NOT NULL auto_increment,
+  `topic_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) unsigned DEFAULT NULL,
   `forum_id` int(11) unsigned NOT NULL,
   `user_id` int(11) unsigned NOT NULL,
   `topic_title` varchar(255) NOT NULL,
   `topic_url` varchar(255) NOT NULL,
   `topic_date` datetime NOT NULL,
-  `topic_status` int(11) unsigned NOT NULL,
-  PRIMARY KEY  (`topic_id`),
+  `topic_status` int(11) NOT NULL DEFAULT '0',
+  `topic_position` int(11) NOT NULL DEFAULT '0',
+  `topic_views` int(11) NOT NULL,
+  `last_post_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`topic_id`),
   KEY `forum_id` (`forum_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=59 ;
-
-INSERT INTO `prefix_forum_topics` (`topic_id`, `forum_id`, `user_id`, `topic_title`, `topic_url`, `topic_date`, `topic_status`) VALUES
-(49, 2, 1, 'Для теста вывода последних тем', 'test', '2011-05-05 00:00:00', 1),
-(50, 3, 1, 'asdasdasdasdasd', 'test', '2011-05-05 00:00:00', 1),
-(51, 3, 1, 'asdasdasdas', 'test', '2011-05-05 00:00:00', 1),
-(52, 3, 1, 'asdasdasdasda', 'test', '2011-05-05 00:00:00', 1),
-(53, 2, 1, 'aaaaaaaaa', 'test', '2011-05-05 00:00:00', 1),
-(55, 3, 1, '1231231', 'test', '2011-05-05 00:00:00', 1),
-(56, 2, 1, '12341234235', 'test', '2011-05-05 00:00:00', 1),
-(58, 1, 1, '123123123', 'test', '2011-05-05 15:20:28', 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
