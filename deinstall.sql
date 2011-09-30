01d9f64b4bea11a87dadace6271198d4d37de9bb
Alex Yelizarov <felex-ae@ya.ru>
2011-09-30 22:49:48 +0400

    Деактивация и удаление таблиц

---
+++ deinstall.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `prefix_forum`;
+DROP TABLE IF EXISTS `prefix_forum_category`;
+DROP TABLE IF EXISTS `prefix_forum_post`;
+DROP TABLE IF EXISTS `prefix_forum_topic`;
+DROP TABLE IF EXISTS `prefix_forum_read`;
